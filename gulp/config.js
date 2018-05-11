0d2545348dadddbab82fddb1c008c451e79b18a6
guozhenyu-tutu <guozhenyu93@sina.com>
2018-05-11 19:48:04 +0800

    首页、购买商品流程、购买列表
    首页、购买商品流程、购买列表

---
+++ gulp/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    //服务器配置
+    serverDev: {
+
+        name: 'jiafu-ticket',//服务名称
+
+        root: 'build',//目录
+
+        host: "192.168.120.151",//ip
+
+        port: 333,//端口
+
+        livereload: true//是否支持实时刷新
+
+    }
+
+};
